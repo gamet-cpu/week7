e7d29b5c481fa3fbdbf824f3f5581230fa327815
gamet-cpu <poihn420@gmail.com>
2025-07-02 12:38:37 +0500

    Add files via upload

---
+++ 32.js
@@ -0,0 +1,66 @@
+// function greet(){
+//     console.log('Hello world');
+// }
+// greet();
+
+// function sayhello(){
+//     let name = prompt()
+//     console.log('Привет,' + name + "!");
+// }
+// sayhello();
+
+
+// let a = Number(prompt());
+// let b = Number(prompt());
+// function sum(a, b){
+//     return a + b;
+// }
+// console.log(sum(a, b))
+
+// let n = Number(prompt())
+// function isEven(n){
+//     if (n % 2 == 0){
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+
+// }
+// console.log(isEven(n));
+
+
+// let a1 = Number(prompt());
+// let b1 = Number(prompt());
+// function max(a1, b1) {
+//     if (a1 > b1) {
+//         return a;
+//     }
+//     else if (b1 > a1 ){
+//         return b1;
+//     }
+//     else {
+//         return "Числа равны";
+//     }
+// }
+// console.log(max(a1, b1))
+
+// let name1 = prompt();
+// let lastname = prompt();
+// function getInitials(name1, lastname){
+//     return name1[0] + "." + lastname[0] + '.';
+// }
+// console.log(getInitials(name1, lastname));
+
+// function square(num) {
+//   return num * num;
+// }
+// function cube(num) {
+//   return num * square(num); // т.е. num * (num * num)
+// }
+
+let a2 = Number(prompt());
+let b2 = Number(prompt());
+const sum = (a2, b2) => a2 + b2;
+
+console.log(sum(a2,b2)); 
